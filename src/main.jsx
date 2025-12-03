f12cbf74a3bb5fe016c844f9fa98130bf3f0423a
victus-owner <ashishprajapati0202@gmail.com>
2025-12-03 16:04:33 +0530

    switch to HashRouter for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+    <App />
+  </HashRouter>
   </StrictMode>
 );
